6ad4c3f2570252877a5f3e79c0cbb3dd0d40ef59
Gadiel Vargas <gadielvargaz@gmail.com>
2024-03-24 20:53:37 -0300

    Add js function for mailto

--- script.js
+++ script.js
@@ -63,3 +63,23 @@ window.requestAnimFrame = (function() {
       scrollToElement( element, 1500, 'easeInOutQuint');
     });
   }
+
+  (function($) {
+    $('a[href^=mailto]').each(function() {
+      var href = $(this).attr('href');
+      $(this).click(function() {
+        var t;
+        var self = $(this);
+  
+        $(window).blur(function() {
+          // The browser apparently responded, so stop the timeout.
+          clearTimeout(t);
+        });
+  
+        t = setTimeout(function() {
+          // The browser did not respond after 500ms, so open an alternative URL.
+          document.location.href = 'mailto:gadielvargaz@gmail.com';
+        }, 500);
+      });
+    });
+  })(jQuery);
